2ecb5e396488ae529dbf1dd40aaf9528febf21f8
smithre <smithre@b2aeb54e-356d-47a7-8c2a-09df85fd4d13>
2010-07-22 12:52:06 +0000

    RES: added check_file_exists

--- modules/ReseqTrack/Tools/FileSystemUtils.pm
+++ modules/ReseqTrack/Tools/FileSystemUtils.pm
@@ -13,22 +13,19 @@ use Time::localtime;
 
 use vars qw (@ISA  @EXPORT);
 
-@ISA = qw(Exporter);
-@EXPORT = qw(list_files_in_dir 
-             get_lines_from_file
-	     get_md5hash
-             get_hash_from_tab_file
-             get_columns_from_tab_file
-             run_md5
-             find_file
-             check_md5
-             dump_dirtree_summary
-             delete_directory );
-
-
-
-
-
+@ISA    = qw(Exporter);
+@EXPORT = qw(
+  list_files_in_dir
+  get_lines_from_file
+  get_md5hash
+  get_hash_from_tab_file
+  get_columns_from_tab_file
+  run_md5
+  find_file
+  check_md5
+  dump_dirtree_summary
+  delete_directory
+  check_files_exists );
 
 =head2 get_filenames
 
@@ -41,17 +38,15 @@ use vars qw (@ISA  @EXPORT);
 
 =cut
 
-
-
-sub get_filenames{
-  my ($dir) = @_;
-  no strict;
-  sub find(&@) { &File::Find::find }
-  *name = *File::Find::name;
-  my @files;
-  find { push(@files, $name) } $dir;
-  use strict;
-  return \@files;
+sub get_filenames {
+ my ($dir) = @_;
+ no strict;
+ sub find(&@) { &File::Find::find }
+ *name = *File::Find::name;
+ my @files;
+ find { push( @files, $name ) } $dir;
+ use strict;
+ return \@files;
 }
 
 =head2 list_files_in_dir
@@ -67,45 +62,45 @@ sub get_filenames{
 
 =cut
 
+sub list_files_in_dir {
+ my ( $dir, $get_full_paths ) = @_;
+ my $name = $File::Find::name;
+ throw( "Must pass list_files_in_dir a directory not " . $dir )
+   unless ( -d $dir );
+ my $list = get_filenames($dir);
 
-sub list_files_in_dir{
-  my ($dir, $get_full_paths) = @_;
-  my $name = $File::Find::name;
-  throw("Must pass list_files_in_dir a directory not ".$dir) unless( -d $dir);
-  my $list = get_filenames($dir);
-  #find { push(@list, $name)} $dir;
-  my %hash;
-  my @files;
-  foreach my $element(@$list){
-    next if(!$element);
-    if(-d $element){
-      if(!$hash{$element}){
-        $hash{$element} = [];
-      }
-    }else{
-      my $file;
-      my $dir_path = dirname($element);
-      if($get_full_paths){
-        $file = $element;
-      }else{
-        $file = basename($element);
-      }
-      my $name = basename($element);
-      next if($name =~ /^\./);
-      push(@files, $file);
-      if(!$hash{$dir_path}){
-        $hash{$dir_path} = [];
-      }
-      push(@{$hash{$dir_path}}, $file);
-    }
+ #find { push(@list, $name)} $dir;
+ my %hash;
+ my @files;
+ foreach my $element (@$list) {
+  next if ( !$element );
+  if ( -d $element ) {
+   if ( !$hash{$element} ) {
+    $hash{$element} = [];
+   }
   }
-      
-  return undef if(@files == 0);
-  return (\@files, \%hash);
+  else {
+   my $file;
+   my $dir_path = dirname($element);
+   if ($get_full_paths) {
+    $file = $element;
+   }
+   else {
+    $file = basename($element);
+   }
+   my $name = basename($element);
+   next if ( $name =~ /^\./ );
+   push( @files, $file );
+   if ( !$hash{$dir_path} ) {
+    $hash{$dir_path} = [];
+   }
+   push( @{ $hash{$dir_path} }, $file );
+  }
+ }
+
+ return undef if ( @files == 0 );
+ return ( \@files, \%hash );
 }
-
-
-
 
 =head2 get_lines_from_file
 
@@ -118,22 +113,22 @@ sub list_files_in_dir{
 
 =cut
 
-sub get_lines_from_file{
-  my ($file) = @_;
-  unless(-e $file){
-    throw($file." must exist in order to be read");
-  }
-  my @return;
-  open(FILE,$file)||throw("Cannot open the file $file");
+sub get_lines_from_file {
+ my ($file) = @_;
+ unless ( -e $file ) {
+  throw( $file . " must exist in order to be read" );
+ }
+ my @return;
+ open( FILE, $file ) || throw("Cannot open the file $file");
 
-  foreach my $line (<FILE>){
-    chomp($line);
-    next if($line =~ /^\s+$/);
-    push @return, $line;
-  }
-  close(FILE) or throw("Failed to close ".$file);
-  warning($file." contained no lines") if(@return == 0);
-  return \@return;
+ foreach my $line (<FILE>) {
+  chomp($line);
+  next if ( $line =~ /^\s+$/ );
+  push @return, $line;
+ }
+ close(FILE) or throw( "Failed to close " . $file );
+ warning( $file . " contained no lines" ) if ( @return == 0 );
+ return \@return;
 }
 
 =head2 get_Md5hash
@@ -150,37 +145,37 @@ sub get_lines_from_file{
 
 =cut
 
+sub get_md5hash {
+ my ( $file, $trim ) = @_;
+ unless ( -e $file ) {
+  throw( $file . " must exist in order to be read" );
+ }
+ open( FH, $file ) or throw( "Failed to open " . $file );
+ my %filehash;
+ my %md5hash;
+ while (<FH>) {
+  chomp;
+  my ( $md5, $filename ) = split;
+  throw( $filename . " has zero md5 in " . $file )
+    if ( !$md5 || ( $md5 =~ /^\d+$/ && $md5 == 0 ) );
+  warning( $filename . " already existing in hash" )
+    if ( $filehash{$filename} );
 
-
-sub get_md5hash{
-  my ($file, $trim) = @_;
-  unless(-e $file){
-    throw($file." must exist in order to be read");
+  #warning($md5." is a duplicate") if($md5hash{$md5});
+  if ( !$md5 || length($md5) == 0 ) {
+   print $_. " doesn't have an md5\n";
   }
-  open(FH, $file) or throw("Failed to open ".$file);
-  my %filehash;
-  my %md5hash;  while(<FH>){
-    chomp;
-    my ($md5, $filename) = split; 
-    throw($filename." has zero md5 in ".$file) if(!$md5 || ($md5 =~ /^\d+$/ && $md5 == 0));
-    warning($filename." already existing in hash") if($filehash{$filename});
-    #warning($md5." is a duplicate") if($md5hash{$md5});
-    if(!$md5 || length($md5) == 0){
-      print $_." doesn't have an md5\n";
-    }
-    if(!$filename || length($filename) == 0){
-      print $_." doesn't have a filename\n";
-    }
-    my $name = $filename;
-    $name = basename($filename) if($trim);
-    $filehash{$name} = $md5;
-    $md5hash{$md5} = $filename;
+  if ( !$filename || length($filename) == 0 ) {
+   print $_. " doesn't have a filename\n";
   }
-  close(FH) or throw("Failed to close ".$file);
-  return \%filehash;
+  my $name = $filename;
+  $name = basename($filename) if ($trim);
+  $filehash{$name} = $md5;
+  $md5hash{$md5}   = $filename;
+ }
+ close(FH) or throw( "Failed to close " . $file );
+ return \%filehash;
 }
-
-
 
 =head2 get_hash_from_tab_file
 
@@ -194,25 +189,21 @@ sub get_md5hash{
 
 =cut
 
-
-
-sub get_hash_from_tab_file{
-  my ($file, $column) = @_;
-  open(FH, $file) or throw("Failed to open ".$file." $!");
-  my %hash;
-  while(<FH>){
-    chomp;
-    my $string = $_;
-    my @values = split /\t/, $string;
-    if(!$hash{$values[$column]}){
-      $hash{$values[$column]} = [];
-    }
-    push(@{$hash{$values[$column]}}, $string);
+sub get_hash_from_tab_file {
+ my ( $file, $column ) = @_;
+ open( FH, $file ) or throw( "Failed to open " . $file . " $!" );
+ my %hash;
+ while (<FH>) {
+  chomp;
+  my $string = $_;
+  my @values = split /\t/, $string;
+  if ( !$hash{ $values[$column] } ) {
+   $hash{ $values[$column] } = [];
   }
-  return \%hash;
+  push( @{ $hash{ $values[$column] } }, $string );
+ }
+ return \%hash;
 }
-
-
 
 =head2 get_columns_from_tab_file
 
@@ -226,30 +217,28 @@ sub get_hash_from_tab_file{
 
 =cut
 
+sub get_columns_from_tab_file {
+ my ( $file, $key_column, $value_column ) = @_;
+ open( FH, $file ) or throw( "Failed to open " . $file . " $!" );
+ my %hash;
+ while (<FH>) {
+  chomp;
+  my $string = $_;
+  my @values = split /\t/, $string;
+  if ( $hash{ $values[$key_column] } ) {
+   warning(   "ReseqTrack::Tools::FileUtils::get_columns_from_tab_file "
+            . "assumes that the contents of "
+            . $key_column . " in "
+            . $file . " are "
+            . "unique but they appear not to be, this will means "
+            . $value_column
+            . "entries in the values of the hash maybe misleading" );
 
-
-
-
-sub get_columns_from_tab_file{
-  my ($file, $key_column, $value_column) = @_;
-   open(FH, $file) or throw("Failed to open ".$file." $!");
-  my %hash;
-  while(<FH>){
-    chomp;
-    my $string = $_;
-    my @values = split /\t/, $string;
-    if($hash{$values[$key_column]}){
-      warning("ReseqTrack::Tools::FileUtils::get_columns_from_tab_file ".
-              "assumes that the contents of ".$key_column." in ".$file." are ".
-              "unique but they appear not to be, this will means ".$value_column.
-              "entries in the values of the hash maybe misleading");
-
-    }
-    $hash{$values[$key_column]} = $values[$value_column];
   }
-  return \%hash;
+  $hash{ $values[$key_column] } = $values[$value_column];
+ }
+ return \%hash;
 }
-
 
 =head2 run_md5
 
@@ -262,22 +251,20 @@ sub get_columns_from_tab_file{
 
 =cut
 
-
-
-sub run_md5{
-  my ($file, $program) = @_;
-  $program = 'md5sum' if(!$program);
-  throw("Can't run md5sum on a non existent file $file") unless (-e $file);
-  my $cmd = $program." ".$file;
-  open(FH, $cmd." | ") or throw("Failed to open ".$cmd);
-  my $md5;
-  while(<FH>){
-    chomp;
-    my @values = split;
-    $md5 = $values[0];
-  }
-  close(FH);
-  return $md5;
+sub run_md5 {
+ my ( $file, $program ) = @_;
+ $program = 'md5sum' if ( !$program );
+ throw("Can't run md5sum on a non existent file $file") unless ( -e $file );
+ my $cmd = $program . " " . $file;
+ open( FH, $cmd . " | " ) or throw( "Failed to open " . $cmd );
+ my $md5;
+ while (<FH>) {
+  chomp;
+  my @values = split;
+  $md5 = $values[0];
+ }
+ close(FH);
+ return $md5;
 }
 
 =head2 find_file
@@ -292,22 +279,19 @@ sub run_md5{
 
 =cut
 
-
-sub find_file{
-  my ($name, $dir, $verbose) = @_;
-  my $cmd = "find $dir -name \"".$name."\" -print";
-  print STDERR $cmd."\n" if($verbose);
-  open(CMD, $cmd." | ") or throw("Failed to open ".$cmd." $!");
-  my @paths;
-  while(<CMD>){
-    chomp;
-    push(@paths, $_);
-  }
-  close(CMD);
-  return \@paths;
+sub find_file {
+ my ( $name, $dir, $verbose ) = @_;
+ my $cmd = "find $dir -name \"" . $name . "\" -print";
+ print STDERR $cmd . "\n" if ($verbose);
+ open( CMD, $cmd . " | " ) or throw( "Failed to open " . $cmd . " $!" );
+ my @paths;
+ while (<CMD>) {
+  chomp;
+  push( @paths, $_ );
+ }
+ close(CMD);
+ return \@paths;
 }
-
-
 
 =head2 check_md5
 
@@ -321,80 +305,89 @@ sub find_file{
 
 =cut
 
-
-sub check_md5{
-  my ($file, $hash) = @_;
-  my $name = basename($file);
-  my $md5 = $hash->{$name};
-  my $calculated_md5 = run_md5($file);
-  unless($md5 eq $calculated_md5){
-    return 0;
-  }else{
-    return 1;
-  }
-  print STDERR "Shouldn't of reached this point in FileUtils check_md5\n";
+sub check_md5 {
+ my ( $file, $hash ) = @_;
+ my $name           = basename($file);
+ my $md5            = $hash->{$name};
+ my $calculated_md5 = run_md5($file);
+ unless ( $md5 eq $calculated_md5 ) {
+  return 0;
+ }
+ else {
+  return 1;
+ }
+ print STDERR "Shouldn't of reached this point in FileUtils check_md5\n";
 }
 
-
-sub dump_dirtree_summary{
-  my ($dir, $output_file, $skip_regex) = @_;
-  $skip_regex = 'current.tree';
-  my ($files, $hash) = list_files_in_dir($dir, 1);
-  my $fh;
-  if($output_file){
-    open(FH, ">".$output_file) or throw("Failed to open ".$output_file." $!");
-    $fh = \*FH;
-  }else{
-    $fh = \*STDOUT;
+sub dump_dirtree_summary {
+ my ( $dir, $output_file, $skip_regex ) = @_;
+ $skip_regex = 'current.tree';
+ my ( $files, $hash ) = list_files_in_dir( $dir, 1 );
+ my $fh;
+ if ($output_file) {
+  open( FH, ">" . $output_file )
+    or throw( "Failed to open " . $output_file . " $!" );
+  $fh = \*FH;
+ }
+ else {
+  $fh = \*STDOUT;
+ }
+ my %dirs;
+ foreach my $file (@$files) {
+  next if ( $file =~ /$skip_regex/ );
+  my $dir = dirname($file);
+  my $label;
+  unless ( $dirs{$dir} ) {
+   my $dir_size  = -s $dir;
+   my $dir_stamp = ctime( stat($dir)->mtime );
+   $label = 'directory';
+   print $fh join( "\t", $dir, $label, $dir_size, $dir_stamp ) . "\n";
+   $dirs{$dir} = 1;
   }
-  my %dirs;
-  foreach my $file(@$files){
-    next if($file =~ /$skip_regex/);
-    my $dir = dirname($file);
-    my $label;
-    unless($dirs{$dir}){
-      my $dir_size = -s $dir;
-      my $dir_stamp = ctime(stat($dir)->mtime);
-      $label = 'directory';
-      print $fh join("\t", $dir, $label, $dir_size, $dir_stamp)."\n";
-      $dirs{$dir} = 1;
-    }
-    my $size = -s $file;
-    my $date_string = ctime(stat($file)->mtime);
-    $label = 'file';
-    print $fh join("\t", $file, $label, $size, $date_string)."\n";
-  }
-  close($fh);
+  my $size        = -s $file;
+  my $date_string = ctime( stat($file)->mtime );
+  $label = 'file';
+  print $fh join( "\t", $file, $label, $size, $date_string ) . "\n";
+ }
+ close($fh);
 }
 
 sub delete_directory {
  my $dir = shift;
- 
+
  my $failed_unlink = 0;
- my $ok = 0; 
- 
- die "$dir is not a directory\n" if (! -d $dir);
- die "$dir does not exist\n"     if (! -e $dir);
- die "Not full path to $dir\n\n" if ( ! ( $dir =~/^\//));
+ my $ok            = 0;
+
+ die "$dir is not a directory\n" if ( !-d $dir );
+ die "$dir does not exist\n"     if ( !-e $dir );
+ die "Not full path to $dir\n\n" if ( !( $dir =~ /^\// ) );
 
  #remove all files first
  my @files = <$dir/*>;
  foreach my $file (@files) {
-   $ok = unlink ($file);
-   if ($ok == 0){
-     print "Failed unlink: $file\n";
-     $failed_unlink = 1;
-   } 
- } 
+  $ok = unlink($file);
+  if ( $ok == 0 ) {
+   print "Failed unlink: $file\n";
+   $failed_unlink = 1;
+  }
+ }
 
- if ($failed_unlink == 0){
-    $ok = rmdir ($dir);
-    print "Deleted $dir\n" if ($ok);
+ if ( $failed_unlink == 0 ) {
+  $ok = rmdir($dir);
+  print "Deleted $dir\n" if ($ok);
  }
 
 }
 
 
-
+sub check_files_exists {
+ my $file = shift;
+ 
+ die "$file is a directory, not a file\n" if ( -d $file );
+ die "$file does not exist\n"     if ( !-e $file );
+ if ( !( $file =~ /^\// ) && -e $file){
+  warn "Not full path to $file. But it exists";
+}
+}
 
 1;
