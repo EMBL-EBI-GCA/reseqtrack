0b0a9c3c69195ac6b07d366a42646eeaf9b8f12a
hollyzheng <hollyzheng@b2aeb54e-356d-47a7-8c2a-09df85fd4d13>
2011-12-01 15:00:30 +0000

    enable -cache mode for variant_pattern_finder.pl and handle indels correctly

--- scripts/variation_data/variant_pattern_finder.pl
+++ scripts/variation_data/variant_pattern_finder.pl
@@ -25,6 +25,7 @@ use strict;
 use Getopt::Long;
 use Bio::EnsEMBL::Registry;
 use Vcf;
+use Bio::EnsEMBL::Variation::Utils::VEP qw(get_all_consequences);
 
 my (	$file,
 		$region,					
@@ -38,22 +39,32 @@ my (	$file,
 		%local_samples,				# this stores all samples with alt alleles in any snps in the region
 		%unique_allele_strings,
 		$help,
+		$cache,
+		$outfile,
 );
 
 my $output_dir = ".";
 my $print_all = 0;
 my $expanded_view = 0;
 my $verbose = 0;
-	
+my $host = 'ensembldb.ensembl.org';
+my $user = 'anonymous';
+my $cache_dir = '/nfs/1000g-work/G1K/work/zheng/.vep/homo_sapiens/64';
+
 &GetOptions( 
+	'host=s'		=>\$host,
+	'user=s'		=>\$user,
 	'vcf=s'			=>\$file,
 	'region=s'		=>\$region,
 	'sample=s'		=>\$sample_panel,
 	'output_dir=s'	=>\$output_dir,
+	'output_file=s'	=>\$outfile,
 	'expanded_view!'	=>\$expanded_view,
 	'print_all!'	=>\$print_all,
 	'help!'			=>\$help,
 	'verbose!'		=>\$verbose,
+	'cache!'		=>\$cache,
+	'caches_dir=s'	=>\$cache_dir,
 );
 
 if ($help) {
@@ -62,8 +73,11 @@ if ($help) {
 
 die("please provide vcf file, chromosomal region, and sample panel file") if (!$file || !$region || !$sample_panel);
 
-my $outfile =  "$output_dir/chr" . $region . ".csv";
-$outfile =~ s/:/_/;
+if (!$outfile) {
+	$outfile =  "$output_dir/chr" . $region . ".csv";
+	$outfile =~ s/:/_/;
+}
+
 open(OUT, ">", $outfile) || die("Cannot open output file $outfile");
 
 my $header = "$output_dir/header";
@@ -77,15 +91,20 @@ $vcf->parse_header();
 while (my $x=$vcf->next_data_hash()) {
         #print join ("\t", keys (%$x)) , "\n"; # to get all possible hash keys:
         #FORMAT  QUAL    ID      CHROM   INFO    FILTER  gtypes  REF     ALT     POS 
+        # $x->{REF} is string
+        # $x->{ALT} is array
+        # use 'perldoc Vcf' to see details of Vcf.pm
         
         my $position = $x->{CHROM}.":".$x->{POS};
-        print  "SNP: $position " . $x->{ID} . " REF IS: " . $x->{REF} . " ALT IS: " . $x->{ALT}->[0] . "\n" if ($verbose);
+        
+        my $alt = join ("", @{$x->{ALT}});
+        print  "SNP: $position " . $x->{ID} . " REF IS: " . $x->{REF} . " ALT IS: " . $alt . "\n" if ($verbose);
 
 		my $identifier = $position . "_" . $x->{ID};
 		
-		my $func_flag = populate_snp_impact_hash($x, $identifier);
+		my $is_functional = populate_snp_impact_hash($x, $identifier, $cache, $cache_dir);
 		
-		next if ($func_flag == 0 && !$print_all);
+		next if ($is_functional == 0 && !$print_all);
 		
         for my $individual (keys %{$$x{gtypes}}) {
             my ($al1,$sep,$al2) = $vcf->parse_alleles($x,$individual);
@@ -215,10 +234,23 @@ sub parse_sample_file {
 }
 
 sub populate_snp_impact_hash {
-	my ($y, $identifier) = @_;
-	my $snp_func_ref = predict_snp_func($y->{CHROM}, $y->{POS}, $y->{REF}, $y->{ALT}->[0]); ### FIXME, do we worry about non-biallelic snps???
-	my %snp_func = %$snp_func_ref;
+	my ($y, $identifier, $cache, $cache_dir1) = @_;
+	
+	my $snp_func_ref;
 	my $flag = 0;	
+	
+	## Make sure indels get the correct ensembl-style coordinates	
+	my ($s, $e, $allele_string) = vcf_to_ensembl($y); ### FIXME, when Will has appropriate documentation for VEP.pm, 
+													  ### will use parse_line function to create VariationFeature objects from VCF line by line.
+	
+	if ( $cache ) {
+		$snp_func_ref = predict_snp_func_in_cache($y->{CHROM}, $s, $e, $allele_string, $cache_dir1);
+	}
+	else {	
+		$snp_func_ref = predict_snp_func($y->{CHROM}, $s, $e, $allele_string,); 
+	}
+	
+	my %snp_func = %$snp_func_ref;
 	foreach my $transcript (keys %snp_func) {
 		foreach my $string (@{$snp_func{$transcript}}) {
 			my ($func, $aa) = split(/:/, $string);
@@ -239,38 +271,86 @@ sub populate_snp_impact_hash {
 	return $flag;
 }        
 
-sub predict_snp_func {
-	my ($chr, $pos, $allele1, $allele2) = @_;
+### To use the cache mode, the ensembl variation archive file needs to be downloaded first at $cache_dir2
+### This uses the Bio::EnsEMBL::Variation::Utils::VEP qw(get_all_consequences)
+sub predict_snp_func_in_cache {
+	my ($chr, $s, $e, $allele_string, $cache_dir2) = @_;
+
+	my ($tr_cache, $rf_cache, $config, %tc_consequence);
+		
+	# spoof config
+	$config = {
+		'dir' => $cache_dir2,
+		'chunk_size' => 50000,
+		'cache_region_size' => 1000000,
+		'terms' => 'display',
+		'cache' => 1,
+		'compress' => 'zcat',
+		'quiet' => 1,
+	};
+		
+	my $new_vf = Bio::EnsEMBL::Variation::VariationFeature->new_fast({
+		start => $s,
+		end => $e,
+		chr => $chr,
+		allele_string => $allele_string,
+		strand => 1,
+		map_weight => 1,
+	});
 	
+	foreach my $line_hash(@{get_all_consequences($config, [$new_vf], $tr_cache, $rf_cache)}) { ##### Necessary to FIXME? get_all_consequences take a listref of many variantionFeatures objects
+																							   ##### consider to input >1 a time			
+		unless ( defined($line_hash->{Amino_acids}) ) {
+			$line_hash->{Amino_acids} = '-';
+		}	
+		
+		my $value = $line_hash->{Consequence}  . ":" .  $line_hash->{Amino_acids};
+		push @{$tc_consequence{$line_hash->{Feature}}}, $value;
+=head			
+		print join ("\t", (
+			$new_vf->variation_name,
+			$line_hash->{Feature},
+			$line_hash->{Consequence},
+			$line_hash->{Amino_acids}
+		));
+		print "\n";
+=cut	
+	}	
+	return \%tc_consequence;
+}
+		
+sub predict_snp_func {
+	my ($chr, $s, $e, $allele_string) = @_;
+		
 	#### get Ensembl API registry ####
 	my $reg = 'Bio::EnsEMBL::Registry';
-	$reg->load_registry_from_db(-host => 'ensembldb.ensembl.org',-user => 'anonymous');
+	$reg->load_registry_from_db(-host =>$host,-user =>$user);
 	
 	my $vfa	= $reg->get_adaptor('human', 'variation', 'variationfeature');
 	my $sa	= $reg->get_adaptor('human', 'core', 'slice');
 
-	my $genotype = $allele1 . "/" . $allele2; ##In SNP predictor, the phase information is not kept.
-	#print "Allels are $genotype\n";	
-	my $identifier = $chr . ":" . $pos;
 	# get a slice for the new feature to be attached to
 	my $slice = $sa->fetch_by_region('chromosome', $chr);
 
 	# create a new VariationFeature object
 	my $new_vf = Bio::EnsEMBL::Variation::VariationFeature->new(
-	  -start => $pos,
-	  -end => $pos,
+	  -start => $s,
+	  -end => $e,
 	  -slice => $slice,           		# the variation must be attached to a slice
-	  -allele_string => $genotype,    	# the first allele should be the reference allele
+	  -allele_string => $allele_string,    	# the first allele should be the reference allele
 	  -strand => 1,						# For 1KG SNPs, use 1
 	  -map_weight => 1,
 	  -adaptor => $vfa,           		# we must attach a variation feature adaptor
-	  -variation_name => $identifier,
+#	  -variation_name => $identifier,
 	);
 
 	# get the consequence types
 	my %tc_string;
 	foreach my $con(@{$new_vf->get_all_TranscriptVariations}) {
 	  foreach my $string(@{$con->consequence_type}) {
+	    unless (defined $con->pep_allele_string ) {
+	        $con->pep_allele_string = '-';
+	    }    
 	    my $value = $string . ":" .  $con->pep_allele_string;
 	  	push @{$tc_string{$con->transcript->stable_id}},  $value;	  	 
 	  }
@@ -287,6 +367,117 @@ sub post_processing {
 	`rm $outfile1`;
 	return 1;
 }
+
+
+#### VCF file records indels differently from ensembl; VCF file only record the start position of the reference allele, 
+#### ensembl actually records start and end of an allele
+#### This sub is adopted from Will McLaren's parse_vcf function in VEP.pm, it conver the VCF indels coordinates to ensembl convention
+#### It also converts non-biallelic alternative alleles to strings separated by '/' rather than ','.  A,G -->A/G
+sub vcf_to_ensembl {
+	
+	my ($z) = @_;
+	
+	my $chr = $z->{CHROM};
+	my $start = $z->{POS};
+	my $end = $z->{POS};
+	my $ref = $z->{REF}; #string
+	my $alt = join('', @{$z->{ALT}});
+	 
+	# adjust end coord
+    $end += (length($ref) - 1);
+   
+    # find out if any of the alt alleles make this an insertion or a deletion
+    my ($is_indel, $is_sub, $ins_count, $total_count);
+    foreach my $alt_allele(split /\,/, $alt) {
+        $is_indel = 1 if $alt_allele =~ /D|I/;
+        $is_indel = 1 if length($alt_allele) != length($ref);
+        $is_sub = 1 if length($alt_allele) == length($ref);
+        $ins_count++ if length($alt_allele) > length($ref);
+        $total_count++;
+    }
+   
+    # multiple alt alleles?
+    if($alt =~ /\,/) {
+        if($is_indel) {
+            my @alts;
+
+            if($alt =~ /D|I/) {
+                foreach my $alt_allele(split /\,/, $alt) {
+                    # deletion (VCF <4)
+                    if($alt_allele =~ /D/) {
+                        push @alts, '-';
+                    }
+
+                    elsif($alt_allele =~ /I/) {
+                        $alt_allele =~ s/^I//g;
+                        push @alts, $alt_allele;
+                    }
+                }
+            }
+
+            else {
+                $ref = substr($ref, 1) || '-';
+                $start++;
+
+                foreach my $alt_allele(split /\,/, $alt) {
+                    $alt_allele = substr($alt_allele, 1);
+                    $alt_allele = '-' if $alt_allele eq '';
+                    push @alts, $alt_allele;
+                }
+            }
+
+            $alt = join "/", @alts;
+        }
+
+        else {
+            # for substitutions we just need to replace ',' with '/' in $alt
+            $alt =~ s/\,/\//;
+        }
+    }
+
+    elsif($is_indel) {
+        # deletion (VCF <4)
+        if($alt =~ /^D/) {
+            my $num_deleted = $alt;
+            $num_deleted =~ s/\D+//g;
+            $end += $num_deleted - 1;
+            $alt = "-";
+            $ref .= ("N" x ($num_deleted - 1)) unless length($ref) > 1;
+        }
+
+        # insertion (VCF <4)
+        elsif($alt =~ /^I/) {    
+            $ref = '-';
+            $alt =~ s/^I//g;
+            $start++;
+        }
+
+        # insertion or deletion (VCF 4+)
+        elsif(substr($ref, 0, 1) eq substr($alt, 0, 1)) { # if the first base is the same for ref and alt, it indicates an indel
+            # chop off first base
+            $ref = substr($ref, 1) || '-';
+            $alt = substr($alt, 1) || '-';
+
+            $start++;
+        }
+        
+        # SV deletion (VCF 4+)
+        elsif($alt =~ /DEL/) {
+            my $num_deleted = length($ref); #### FIXME, not perfect solution. Wait for Will's code in ensembl release 65
+            $end += $num_deleted - 1;
+            $alt = "-";
+            $ref .= ("N" x ($num_deleted - 1)) unless length($ref) > 1;
+        }
+        
+        elsif($alt =~ /INS/ ) { #### FIXME
+        
+        }    
+    }
+	
+	my $allele = $ref.'/'.$alt;
+	return ($start, $end, $allele);
+    
+}    
 	
 ##################################################################################################################################
 
@@ -324,7 +515,10 @@ sub post_processing {
 
 =head1	OPTIONAL ARGUMENTS
 
+	-host			Ensembl database host to which you want to connection, default is 'ensembldb.ensembl.org'
+	-user			user name to connect to Ensembl database; default is 'anonymous'
 	-output_dir		Directory where you want the output file be 
+	-output_file		Output file name, if not specified, the script will create output file name based on rule below 
 	-print_all		By default, only variants that change a coding sequence would be considered. If -print_all is set, all variants will be considered. 
 	-expanded_view		By default -expanded_view is OFF and the finder does not distinguish sites of homozygous reference with those with no data, 
 				therefore the number of distinctive combinations of variations is minimized; it offers a simplified and clear variation landscape 
@@ -332,10 +526,12 @@ sub post_processing {
 				data with more accuracy. 
 	-verbose		print progress along the way				
 	-help			Print out help menu
-
+	-cache			Ensembl variation archive file needs to be downloaded before hand to run in the cache mode, see http://www.ensembl.org/info/docs/variation/vep/vep_script.html#cacheopt for details
+	-cache_dir		Directory where the Ensembl archive file is
+			
 =head1	OUTPUT FILE
 
-	The output file is a tab delimited file. It can be found in the output_dir with this naming convention: chr_start-end.csv.sorted. 
+	The output file is a tab delimited file. If user did not specify an output file name, an output file can be found in the output_dir with this naming convention: chr_start-end.csv.sorted. 
 	An example is chr17_41256206-41257206.csv.sorted.  It has two lines of headers.
 
 	Header line 1,		chromosome and chromosomal position of the variation, separated by ":", followed by variation rs number and the reference allele in a 
@@ -368,3 +564,11 @@ perl ~/ReseqTrack/scripts/variation_data/variant_pattern_finder.pl \
 -vcf /nfs/1000g-archive/vol1/ftp/release/20100804/ALL.2of4intersection.20100804.genotypes.vcf.gz \
 -sample /nfs/1000g-archive/vol1/ftp/release/20100804/20100804.ALL.panel \
 -region  17:41256206-41256906
+
+perl ~/ReseqTrack/scripts/variation_data/variant_pattern_finder.pl \
+-vcf /nfs/1000g-archive/vol1/ftp/release/20110521/ALL.chr14.phase1_intergrated_calls.20101123.snps_indels_svs.genotypes.vcf.gz \
+-sample /nfs/1000g-archive/vol1/ftp/release/20110521/phase1_integrated_calls.20101123.ALL.panel \
+-region 14:106329408-106329468 \
+-verbose  \
+-cache \
+-print_all
