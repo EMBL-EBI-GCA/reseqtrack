5f735f9b4fc408fbc92f13d0ea778e7964ddd342
istreeter <istreeter@users.noreply.github.com>
2012-02-29 16:58:05 +0000

    better handling of vcf and tabix

--- scripts/variation_data/vcf_to_ped_convert.pl
+++ scripts/variation_data/vcf_to_ped_convert.pl
@@ -25,7 +25,6 @@
 use Getopt::Long;
 use Net::FTP;
 use Env qw( @PATH );
-use Data::Dumper;
 
 use strict;
 use warnings;
@@ -57,9 +56,19 @@ if ($help) {
 }
 
 die("required arguments: vcf, sample_panel_file, region, population") if (! $vcf || ! $sample_panel || ! $region || ! @populations);
-die("cannot find executable $tabix") if (! -x $tabix && ! grep {-x "$_/$tabix"} @PATH);
 die("$output_dir is not a directory") if ($output_dir && ! -d $output_dir);
-die("$vcf is not a compressed vcf file") if ($vcf !~ /\.vcf\.b?gz(ip)?$/);
+
+my $is_compressed = $vcf =~ /\.g?gz(ip)?$/;
+die("cannot find executable $tabix") if ($is_compressed && ! -x $tabix && ! grep {-x "$_/$tabix"} @PATH);
+die("remote vcf file must be compressed by bgzip") if (!$is_compressed && $vcf =~ /ftp:\/\//);
+
+my ($region_chromosome, $region_start, $region_end);
+if ($region =~ /^(\w+):(\d+)-(\d+)$/) {
+  ($region_chromosome, $region_start, $region_end) = ($1, $2, $3);
+}
+else {
+  die("did not recognise region $region");
+}
 
 if (! $output_ped) {
     $output_ped = "$region.ped";
@@ -77,29 +86,25 @@ if ($output_dir) {
     $output_info =~ s{//}{/}g;
 }
 
+my %individuals;
+my @markers;
+my %genotypes;
 
-my $individuals = get_individuals($sample_panel, \@populations);
-my ($markers, $genotypes) = get_markers_genotypes($vcf, $region, $tabix, $individuals);
+get_individuals();
+get_markers_genotypes();
 
-print_info($markers, $output_info);
-print_ped($genotypes, $output_ped);
+print_info();
+print_ped();
 
 print "Created ".$output_info." and ".$output_ped."\n";
 
 
 
-
-
-
 sub get_markers_genotypes {
-    my ($vcf, $region, $tabix, $individuals) = @_;
-
     my %base_codes = ('A' => 1, 'C' => 2, 'G' => 3, 'T' => 4);
 
-    my @markers;
-    my %genotypes;
-
-    open my $VCF, "$tabix -h $vcf $region |"
+    my $vcf_opener = $is_compressed ? "$tabix -h $vcf $region |" : "<$vcf";
+    open my $VCF, $vcf_opener
         or die("cannot open vcf $!");
 
     my %column_indices;
@@ -118,15 +123,18 @@ sub get_markers_genotypes {
         }
 
         my ($chromosome, $position, $name, $ref_allele, $alt_alleles) = @columns;
+        next LINE if ($chromosome ne $region_chromosome);
+        next LINE if ($position < $region_start);
+        last LINE if ($position > $region_end);
 
         my @allele_codes = map {$base_codes{$_} || 0} $ref_allele, (split(/,/, $alt_alleles));
         next LINE if ((scalar grep {$_} @allele_codes) < 2);
 
         my %marker_genotypes;
         my %alleles_present;
-        foreach my $population (keys %$individuals) {
+        foreach my $population (keys %individuals) {
             INDIVIDUAL:
-            foreach my $individual (@{$individuals->{$population}}) {
+            foreach my $individual (@{$individuals{$population}}) {
                 next INDIVIDUAL if (! $column_indices{$individual});
                 my $genotype_string = $columns[ $column_indices{$individual} ];
                 if ($genotype_string =~ /(\d+)(?:\/|\|)(\d+)/) {
@@ -157,23 +165,25 @@ sub get_markers_genotypes {
     }
 
     close $VCF;
-    my $exit_status = $? >>8;
-    die("tabix exited with status $exit_status") if $exit_status;
 
-    return \@markers, \%genotypes;
+    if ($is_compressed) {
+      my $exit_status = $? >>8;
+      die("tabix exited with status $exit_status") if $exit_status;
+    }
+
+    return;
 }
 
 sub print_ped {
-    my ($genotypes, $file) = @_;
 
-    open my $FILE, '>', $file
-        or die "cannot open $file $!";
-    foreach my $population (keys %$genotypes) {
+    open my $FILE, '>', $output_ped
+        or die "cannot open $output_ped $!";
+    foreach my $population (keys %genotypes) {
         my $pedigree_counter = 1;
-        foreach my $individual (keys %{$genotypes->{$population}}) {
+        foreach my $individual (keys %{$genotypes{$population}}) {
             my $pedigree = $population . '_' . $pedigree_counter;
             print $FILE join("\t", $pedigree, $individual, 0, 0, 0, 0,);
-            foreach my $genotype_codes (@{$genotypes->{$population}->{$individual}}) {
+            foreach my $genotype_codes (@{$genotypes{$population}->{$individual}}) {
                 print $FILE "\t", $genotype_codes->[0], ' ', $genotype_codes->[1];
             }
             print $FILE "\n";
@@ -181,14 +191,14 @@ sub print_ped {
         }
     }
     close $FILE;
+    return;
 }
 
 sub print_info {
-    my ($markers, $file) = @_;
 
-    open my $FILE, '>', $file
-        or die "cannot open $file $!";
-    foreach my $marker (@$markers) {
+    open my $FILE, '>', $output_info
+        or die "cannot open $output_info $!";
+    foreach my $marker (@markers) {
         print $FILE join("\t", @$marker), "\n";
     }
     close $FILE;
@@ -199,7 +209,6 @@ sub print_info {
 
 
 sub get_individuals {
-    my ($sample_panel, $allowed_pops) = @_;
 
     my @sample_panel_lines;
 
@@ -226,8 +235,7 @@ sub get_individuals {
     }
 
     my %allowed_pops_hash;
-    my %individuals;
-    foreach my $pop (@$allowed_pops) {
+    foreach my $pop (@populations) {
         $allowed_pops_hash{$pop} = 1;
         $individuals{$pop} = [];
     }
@@ -238,7 +246,6 @@ sub get_individuals {
             push(@{$individuals{$population}}, $individual);
         }
     }
-    return \%individuals;
 }
 
 
@@ -261,8 +268,8 @@ sub get_individuals {
 
 =head1	REQUIRED ARGUMENTS
 
-	-vcf		    Path to a locally or remotely accessible tabix indexed vcf file.
-                            The vcf file must be compressed by bgzip and indexed by tabix.
+	-vcf		    Path to a locally or remotely accessible vcf file.
+                            The vcf file must be compressed by bgzip and indexed by tabix if it is a remote file.
                             The vcf format is a tab format for presenting variation sites and 
 			    genotypes data and is described at http://vcftools.sourceforge.net/specs.html.
                             This tool takes both vcf4.0 and vcf4.1 format files.
