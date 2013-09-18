b341f600cf2bc86f79434fa6c59e55c25e0cd70b
hollyzheng <HollyZB@users.noreply.github.com>
2013-09-18 16:39:36 +0000

    added functionalities to BAM QA using short indels

--- modules/ReseqTrack/Tools/QC/ChkIndelsBAM.pm
+++ modules/ReseqTrack/Tools/QC/ChkIndelsBAM.pm
@@ -5,9 +5,11 @@ use warnings;
 use vars qw(@ISA);
 use ReseqTrack::Tools::Exception qw(throw warning);
 use ReseqTrack::Tools::Argument qw(rearrange);
+use ReseqTrack::Tools::FileSystemUtils qw(get_lines_from_file);
 use File::Path;
 use File::Basename;
 use base qw(ReseqTrack::Tools::RunProgram);
+use ReseqTrack::DBSQL::StatisticsAdaptor;
 
 =pod
 
@@ -20,11 +22,20 @@ ReseqTrack::Tools::QC::ChkIndelsBAM
 Object for running Heng's chk_indel_rg code, which checks BAM files for excessive amount of short indels, which indicates 
 low data quality.
 
+The output file of the module is input_file.out in the outdir specified. The header of the out put file is:
+file name, RG-ID, #<=6bp-ins, #<=6bp-del, #>6bp-ins and #>6bp-del, 
+
+All results are also stored in the statistic table. 
+
+For 1KG BAMs, anything with ratio of col3/col4 or col4/col3 > 5 is classified as bad. The results can be queried using script query_chk_indel_results.pl
+
 =head1 Example
 
 my $chk_indels = ReseqTrack::Tools::QC::ChkIndelsBAM (
-                      -input_files => '/path/to/file',
-                      -working_dir	=> '/path/to/output_dir
+					  -db				=> $db,
+                      -input_files 		=> '/path/to/file',
+                      -working_dir		=> '/path/to/output_dir
+                      -store			=> $store,
                       );
 $chk_indels->run;
 my $output_file = $chk_indels->output_files->[0];
@@ -35,6 +46,9 @@ my $output_file = $chk_indels->output_files->[0];
 sub new {
 	my ( $class, @args ) = @_;
 	my $self = $class->SUPER::new(@args);
+	my ($db, $store) = rearrange([qw(DB STORE)], @args);
+	$self->db($db);
+	$self->store($store);
 	return $self;
 }
 
@@ -51,7 +65,72 @@ sub run_program {
 	
 	$self->output_files($outfile);
 	
+	$self->_insert_results_into_db;
     return $self;
+}
+
+sub _insert_results_into_db {
+	my ($self) = @_;
+    
+	my $rmi_a = $self->db->get_RunMetaInfoAdaptor;
+	my $stats_a = $self->db->get_StatisticsAdaptor;
+	
+	my $result_file = $self->output_files->[0];
+	my $lines = get_lines_from_file($result_file);
+
+	foreach my $line ( @$lines ) {
+		
+		my @data = split (/\t/, $line);
+		my $rmi_obj = $rmi_a->fetch_by_run_id($data[1]);	
+				
+		my $stats_obj_1 = ReseqTrack::Statistic->new(
+			-table_name => 'run_meta_info',
+			-other_id	=> $rmi_obj->dbID,
+			-attribute_name	=> 'sh_insert',
+			-attribute_value	=> $data[2],
+		);	
+		my $stats_obj_2 = ReseqTrack::Statistic->new(
+			-table_name => 'run_meta_info',
+			-other_id	=> $rmi_obj->dbID,
+			-attribute_name	=> 'sh_del',
+			-attribute_value	=> $data[3],
+		);	
+		my $stats_obj_3 = ReseqTrack::Statistic->new(
+			-table_name => 'run_meta_info',
+			-other_id	=> $rmi_obj->dbID,
+			-attribute_name	=> 'reg_insert',
+			-attribute_value	=> $data[4],
+		);	
+		my $stats_obj_4 = ReseqTrack::Statistic->new(
+			-table_name => 'run_meta_info',
+			-other_id	=> $rmi_obj->dbID,
+			-attribute_name	=> 'reg_del',
+			-attribute_value	=> $data[5],
+		);	
+			
+		$stats_a->store($stats_obj_1) if $self->store;	
+		$stats_a->store($stats_obj_2) if $self->store;	
+		$stats_a->store($stats_obj_3) if $self->store;	
+		$stats_a->store($stats_obj_4) if $self->store;	
+		
+	}	
+		return $self;
+}
+
+sub db{
+  my ($self, $db) = @_;
+  if($db){
+    $self->{db} = $db;
+  }
+  return $self->{db};
+}
+
+sub store{
+  my ($self, $store) = @_;
+  if($store){
+    $self->{store} = $store;
+  }
+  return $self->{store};
 }
 
 1;
