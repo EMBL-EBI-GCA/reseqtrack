faa2c27b3398bd588a7148b70043dbd121e67c0a
daverichardson <daverichardson@b2aeb54e-356d-47a7-8c2a-09df85fd4d13>
2013-12-13 11:47:19 +0000

    updated table script, added views script

--- branches/streeter/hive_meta_refactor_merge/sql/table.sql
+++ branches/streeter/hive_meta_refactor_merge/sql/table.sql
@@ -27,7 +27,7 @@ CREATE TABLE host(
 create table history(
    history_id int(10) unsigned NOT NULL AUTO_INCREMENT,
    other_id int(10) unsigned NOT NULL,
-   table_name enum('file', 'collection', 'event', 'run_meta_info', 'alignment_meta_info', 'pipeline'),
+   table_name enum('file','collection','event','run_meta_info','alignment_meta_info','study','sample','experiment','run','pipeline') NOT NULL,
    comment VARCHAR(65000) NOT NULL,  
    time   datetime NOT NULL, 
    PRIMARY KEY(history_id), 
@@ -183,18 +183,6 @@ CREATE TABLE job_status (
   KEY (status),
   KEY (is_current)
 ) ENGINE=MYISAM;
-
-CREATE TABLE statistics(
-   statistics_id int(10) unsigned NOT  NULL AUTO_INCREMENT,
-   table_name  enum('file', 'event', 'run_meta_info', 'alignment_meta_info', 'collection'),
-   other_id int(10) unsigned NOT  NULL,      
-   attribute_name VARCHAR(50) NOT NULL,
-   attribute_value VARCHAR(255) NOT NULL,
-   PRIMARY KEY (statistics_id),
-   key(attribute_name),
-   key(other_id, table_name),
-   unique(other_id, table_name, attribute_name, attribute_value)
-) ENGINE=MYISAM; 
 
 CREATE TABLE archive(
        archive_id int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -415,8 +403,102 @@ CREATE TABLE pipeline_output(
        PRIMARY KEY(pipeline_output_id)
 ) ENGINE=MYISAM;
 
+CREATE TABLE attribute(
+   attribute_id int(10) unsigned NOT  NULL AUTO_INCREMENT,
+   table_name enum('file','event','run_meta_info','alignment_meta_info','collection','run','sample','experiment','study','pipeline_seed') not null,
+   other_id int(10) unsigned NOT  NULL,      
+   attribute_name VARCHAR(100) NOT NULL,
+   attribute_value VARCHAR(4000) NOT NULL,
+   PRIMARY KEY (attribute_id),
+   key(attribute_name),
+   key(other_id, table_name),
+   unique(other_id, table_name, attribute_name)
+) ENGINE=MYISAM; 
 
+insert into attribute (attribute_id,table_name,other_id,attribute_name,attribute_value)
+select statistics_id,table_name,other_id,attribute_name,attribute_value from statistics;
 
+create table study(
+    study_id int(10) unsigned primary key auto_increment, 
+    study_source_id  varchar(15) not null,
+    status varchar(50) not null ,
+    md5     varchar(32),
+    type      varchar(100) not null ,
+    submission_id varchar(15),
+    submission_date datetime,
+    title varchar(2000),
+    study_alias varchar(500)
+) ENGINE=MYISAM;
+
+create unique index study_src_idx on study(study_source_id);
+
+create table experiment (
+    experiment_id int(10) unsigned primary key auto_increment,
+    experiment_source_id varchar(15) not null,
+    study_id  int(10) unsigned not null ,
+    status varchar(50) not null ,
+    md5     varchar(32),
+    center_name           varchar(100) ,
+    experiment_alias      varchar(500) ,
+    instrument_platform   varchar(50) not null ,
+    instrument_model      varchar(50) ,
+    library_layout        varchar(50) not null ,
+    library_name          varchar(500) ,
+    library_strategy      varchar(50) ,
+    library_source        varchar(50) not null ,
+    library_selection     varchar(50) not null ,
+    paired_nominal_length int(10) ,
+    paired_nominal_sdev   int(10),
+    submission_id varchar(15),
+    submission_date datetime,
+    constraint foreign key (study_id) references study(study_id)
+) ENGINE=MYISAM;
+
+create unique index experiment_src_idx on experiment(experiment_source_id);
+create index experiment_fk1 on experiment(study_id);
+
+create table sample
+(
+    sample_id int(10) unsigned primary key auto_increment,
+    sample_source_id varchar(15) not null,
+    status varchar(50),
+    md5     varchar(32),
+    center_name     varchar(100) ,
+    sample_alias    varchar(500) ,
+    tax_id          varchar(15) ,
+    scientific_name varchar(500) ,
+    common_name     varchar(4000) ,
+    anonymized_name varchar(4000) ,
+    individual_name varchar(4000) ,
+    submission_id varchar(15),
+    submission_date datetime,
+    sample_title varchar(4000)
+) ENGINE=MYISAM;
+
+create unique index sample_src_idx on sample(sample_source_id);
+
+create table run
+(
+    run_id        int(10) unsigned primary key auto_increment,
+    run_source_id	varchar(15) not null,
+    experiment_id  int(10) unsigned not null,
+    sample_id  int(10) unsigned not null,
+    run_alias varchar(500) not null ,
+    status varchar(50) not null ,
+    md5 varchar(32),
+    center_name         varchar(100),
+    run_center_name     varchar(100),
+    instrument_platform varchar(50),
+    instrument_model    varchar(50),
+    submission_id varchar(15),
+    submission_date datetime,
+    constraint foreign key (sample_id) references sample(sample_id),
+    constraint foreign key (experiment_id) references experiment(experiment_id)
+  ) ENGINE=MYISAM;
+
+create index run_fk1 on run(sample_id);
+create index run_fk2 on run(experiment_id);
+create unique index run_src_idx on run(run_source_id);
 
 #Now to add entries to the two standard tables
 
