76937a6ee298933961fd0e8c7b8a37d2ef381bb1
David Richardson <davidrichardson@users.noreply.github.com>
2014-01-02 10:47:11 +0000

    Issue warning if load_from_ena.pl is run without load_new update_existing or force_update

--- scripts/metadata/load_from_ena.pl
+++ scripts/metadata/load_from_ena.pl
@@ -4,7 +4,7 @@ use warnings;
 
 use Getopt::Long;
 
-use ReseqTrack::Tools::Exception qw(throw);
+use ReseqTrack::Tools::Exception qw(throw warning);
 use ReseqTrack::DBSQL::DBAdaptor;
 use ReseqTrack::Tools::ERAUtils qw(get_erapro_conn);
 
@@ -31,30 +31,30 @@ my @target_types;
 my $log_dir;
 
 &GetOptions(
-  'dbhost=s'                  => \$db_params{-host},
-  'dbname=s'                  => \$db_params{-dbname},
-  'dbuser=s'                  => \$db_params{-user},
-  'dbpass=s'                  => \$db_params{-pass},
-  'dbport=s'                  => \$db_params{-port},
-  'era_dbuser=s'              => \$era_params[0],
-  'era_dbpass=s'              => \$era_params[1],
-  'new_study=s'               => \@studies_to_add,
-  'load_new!'                 => \$load_new,
-  'update_existing!'          => \$update_existing,
-  'help!'                     => \$help,
-  'verbose!'                  => \$verbose,
-  'force_update!'             => \$force_update,
-  'add_in=s'                  => \@add_in_modules,
-  'clob_read_length=i'        => \$clob_read_length,
-  'skip_run_stats!'           => \$skip_run_stats,
-  'study=s'                   => \@target_studies,
-  'type=s'                    => \@target_types,
-  'seed_from_old_study_table' => \$seed_from_old_study_table,
-  'log_dir=s'                 => \$log_dir,
+    'dbhost=s'                  => \$db_params{-host},
+    'dbname=s'                  => \$db_params{-dbname},
+    'dbuser=s'                  => \$db_params{-user},
+    'dbpass=s'                  => \$db_params{-pass},
+    'dbport=s'                  => \$db_params{-port},
+    'era_dbuser=s'              => \$era_params[0],
+    'era_dbpass=s'              => \$era_params[1],
+    'new_study=s'               => \@studies_to_add,
+    'load_new!'                 => \$load_new,
+    'update_existing!'          => \$update_existing,
+    'help!'                     => \$help,
+    'verbose!'                  => \$verbose,
+    'force_update!'             => \$force_update,
+    'add_in=s'                  => \@add_in_modules,
+    'clob_read_length=i'        => \$clob_read_length,
+    'skip_run_stats!'           => \$skip_run_stats,
+    'study=s'                   => \@target_studies,
+    'type=s'                    => \@target_types,
+    'seed_from_old_study_table' => \$seed_from_old_study_table,
+    'log_dir=s'                 => \$log_dir,
 );
 
 if ($help) {
-  usage();
+    usage();
 }
 
 my $era_db = get_erapro_conn(@era_params);
@@ -64,75 +64,83 @@ my $reseq_db = ReseqTrack::DBSQL::DBAdaptor->new(%db_params);
 $reseq_db->dbc->db_handle->{'AutoCommit'} = 0;
 
 if ($seed_from_old_study_table) {
-  my $studyIDAdaptor = $reseq_db->get_StudyIDAdaptor();
-  my $study_ids      = $studyIDAdaptor->fetch_all();
-  push @studies_to_add, @$study_ids;
+    my $studyIDAdaptor = $reseq_db->get_StudyIDAdaptor();
+    my $study_ids      = $studyIDAdaptor->fetch_all();
+    push @studies_to_add, @$study_ids;
 }
 
 my $log_fh;
 if ($log_dir) {
-  my $ident         = int( rand(10000) );
-  my $date          = current_date();
-  my $tmp_name      = "update_runmetainfo." . $date . "." . $ident . ".$$.log";
-  my $log_file_path = $log_dir . "/" . $tmp_name;
-  open( $log_fh, '>', $log_file_path )
-    or die("Could not write to $log_file_path: $!");
+    my $ident    = int( rand(10000) );
+    my $date     = current_date();
+    my $tmp_name = "update_runmetainfo." . $date . "." . $ident . ".$$.log";
+    my $log_file_path = $log_dir . "/" . $tmp_name;
+    open( $log_fh, '>', $log_file_path )
+      or die("Could not write to $log_file_path: $!");
 }
 else {
-  $log_fh = *STDOUT;
+    $log_fh = *STDOUT;
 }
 
 my @add_ins;
 my $have_clash_check = undef;
 for my $module (@add_in_modules) {
-  my $file = "$module.pm";
-  $file =~ s{::}{/}g;
-  eval { require "$file"; };
-  if ($@) {
-    throw("cannot load $file: $@");
-  }
+    my $file = "$module.pm";
+    $file =~ s{::}{/}g;
+    eval { require "$file"; };
+    if ($@) {
+        throw("cannot load $file: $@");
+    }
 
-  my $add_in = $module->new(
-    -era_db   => $era_db,
-    -reseq_db => $reseq_db,
-    -log_fh   => $log_fh
-  );
-  push @add_ins, $add_in;
+    my $add_in = $module->new(
+        -era_db   => $era_db,
+        -reseq_db => $reseq_db,
+        -log_fh   => $log_fh
+    );
+    push @add_ins, $add_in;
 
-  if ( $add_in->isa("ReseqTrack::Tools::Metadata::BaseMetaDataClashCheck") ) {
-    $have_clash_check = 1;
-  }
+    if ( $add_in->isa("ReseqTrack::Tools::Metadata::BaseMetaDataClashCheck") ) {
+        $have_clash_check = 1;
+    }
 }
 if ( !$have_clash_check ) {
-  push @add_ins,
-    ReseqTrack::Tools::Metadata::BaseMetaDataClashCheck->new(
-    -era_db   => $era_db,
-    -reseq_db => $reseq_db,
-    -log_fh   => $log_fh
-    );
+    push @add_ins,
+      ReseqTrack::Tools::Metadata::BaseMetaDataClashCheck->new(
+        -era_db   => $era_db,
+        -reseq_db => $reseq_db,
+        -log_fh   => $log_fh
+      );
 }
 
 my $updater = ReseqTrack::Tools::UpdateMetaData->new(
-  -dcc_db          => $reseq_db,
-  -era_db          => $era_db,
-  -verbose         => $verbose,
-  -log_fh          => $log_fh,
-  -add_ins         => \@add_ins,
-  -target_types    => \@target_types,
-  -target_studies  => \@target_studies,
-  -use_default_rsm => !$skip_run_stats,
+    -dcc_db          => $reseq_db,
+    -era_db          => $era_db,
+    -verbose         => $verbose,
+    -log_fh          => $log_fh,
+    -add_ins         => \@add_ins,
+    -target_types    => \@target_types,
+    -target_studies  => \@target_studies,
+    -use_default_rsm => !$skip_run_stats,
 );
 
 $updater->load_new_studies(@studies_to_add) if (@studies_to_add);
-$updater->update_from_era( $load_new, $update_existing, $force_update );
+
+if ( $load_new || $update_existing || $force_update ) {
+    $updater->update_from_era( $load_new, $update_existing, $force_update );
+}
+else {
+    warning(
+"Script run without -load_new , -update_existing or -force_update . This is not normally useful."
+    );
+}
 
 $reseq_db->dbc->db_handle->commit();
 
 $updater->report();
 
 sub usage {
-  exec( 'perldoc', $0 );
-  exit(0);
+    exec( 'perldoc', $0 );
+    exit(0);
 }
 
 =pod
