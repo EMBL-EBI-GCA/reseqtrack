d67fad4bcde8ec87c4006294a8e12f6894c7b185
istreeter <istreeter@b2aeb54e-356d-47a7-8c2a-09df85fd4d13>
2014-01-30 14:35:10 +0000

    split fastq support for fastq named after run_alias

--- trunk/modules/ReseqTrack/Hive/Process/SplitFastq.pm
+++ trunk/modules/ReseqTrack/Hive/Process/SplitFastq.pm
@@ -33,9 +33,9 @@ sub run {
     my $run_source_id = $self->param_required('run_source_id');
     my $run_alias = $self->param('run_alias');
     my $search_string = ($run_source_id && $run_alias) ? "$run_source_id|$run_alias" : $run_source_id || $run_alias;
-    my @regexs = (qr/(?:$search_string})_1\.(?:\w+\.)*f(?:ast)?q(?:\.gz)?/i,
-                  qr/(?:$search_string})_2\.(?:\w+\.)*f(?:astq)?(?:\.gz)?/i,
-                  qr/(?:$search_string})\.(?:\w+\.)*f(?:ast)?q(?:\.gz)?/i);
+    my @regexs = (qr/(?:${search_string})_1\.(?:\w+\.)*f(?:ast)?q(?:\.gz)?/i,
+                  qr/(?:${search_string})_2\.(?:\w+\.)*f(?:astq)?(?:\.gz)?/i,
+                  qr/(?:${search_string})\.(?:\w+\.)*f(?:ast)?q(?:\.gz)?/i);
 
 
     my $fastqs = $self->param_as_array('fastq');
